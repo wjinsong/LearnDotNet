a6835e6152cbd65cf1ae0a24f0fb3965b1e91a76
wjinsong <wjinsong@gmail.com>
2015-04-21 23:17:14 +0800

    INIT

---
+++ LearnDotNet/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
