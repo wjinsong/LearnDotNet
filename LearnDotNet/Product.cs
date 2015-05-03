b656ee0ecc8b46310f1b9233f724f6289cee2ad1
wjinsong <wjinsong@gmail.com>
2015-05-03 20:54:18 +0800

    20150503

--- LearnDotNet/Product.cs
+++ LearnDotNet/Product.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace LearnDotNet
 {
@@ -26,10 +27,49 @@ namespace LearnDotNet
                 this.ProduceDate.ToString("yyyy-M-d").PadLeft(13));
         }
 
+        public static ProductCollection GetSampleCollection()
+        {
+            ProductCollection colletion = new ProductCollection(
+                new Product{ Id =1, Name="Torres Coronas", Price=1000, Code="1001", Category= "Red wine", ProduceDate=DateTime.Parse("1977-12-8")});
+            return colletion;
+        }
+
     }
-    public class ProductCollection
+    public class ProductCollection: IEnumerable<Product>
     {
-        private List<Product> list = new List<Product>();
+        //private List<Product> list = new List<Product>();
+
+        private Hashtable table;
+        public ProductCollection()
+        {
+            table = new Hashtable();
+
+        }
+
+        public ProductCollection(params Product[] array)
+        {
+            table = new Hashtable();
+            foreach (Product item in array)
+            {
+                this.Add(item);
+            }
+        }
+        public Product this[int index]
+        { get; set; }
+        
+        public Product this[string key]
+        { get; set; }
+
+        public void Add(Product item)
+        { 
+        }
+        
+        public int Count { get; }
+
+        public void Clear()
+        {
+            table = new Hashtable();
+        }
 
     }
 
